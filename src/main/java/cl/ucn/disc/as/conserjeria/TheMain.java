735f6ef0b3d1f67080e8c7d9d56bb276a8ed026c
godiecl <mika.cl@gmail.com>
2023-11-22 14:02:14 -0300

    feat: grpc server and implementation

--- src/main/java/cl/ucn/disc/as/conserjeria/TheMain.java
+++ src/main/java/cl/ucn/disc/as/conserjeria/TheMain.java
@@ -4,10 +4,15 @@
 
 package cl.ucn.disc.as.conserjeria;
 
+import cl.ucn.disc.as.conserjeria.grpc.PersonaGrpcServiceImpl;
 import cl.ucn.disc.as.conserjeria.ui.ApiRestServer;
 import cl.ucn.disc.as.conserjeria.ui.WebController;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
 import io.javalin.Javalin;
 import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
 
 /**
  * The Main.
@@ -22,15 +27,31 @@ public final class TheMain {
      *
      * @param args to use.
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException, IOException {
 
         log.debug("Starting Main with library path: {}", System.getProperty("java.library.path"));
 
         // Start the API REST server
+        log.debug("Starting ApiRest server ..");
         Javalin app = ApiRestServer.start(7070, new WebController());
 
         // stop the API REST server.
         // app.stop();
+
+        // Start the gRPC server
+        log.debug("Starting the gRPC server ..");
+        Server server = ServerBuilder
+                .forPort(50123)
+                .addService(new PersonaGrpcServiceImpl())
+                .build();
+
+        server.start();
+
+        // shutdown hook
+        Runtime.getRuntime().addShutdownHook(new Thread(server::shutdown));
+
+        // wait for the stop
+        server.awaitTermination();
 
         log.debug("Done. :)");
 
--- src/main/java/cl/ucn/disc/as/conserjeria/ui/WebController.java
+++ src/main/java/cl/ucn/disc/as/conserjeria/ui/WebController.java
@@ -4,10 +4,16 @@
 
 package cl.ucn.disc.as.conserjeria.ui;
 
+import cl.ucn.disc.as.conserjeria.grpc.PersonaGrpc;
+import cl.ucn.disc.as.conserjeria.grpc.PersonaGrpcRequest;
+import cl.ucn.disc.as.conserjeria.grpc.PersonaGrpcResponse;
+import cl.ucn.disc.as.conserjeria.grpc.PersonaGrpcServiceGrpc;
 import cl.ucn.disc.as.conserjeria.model.Persona;
 import cl.ucn.disc.as.conserjeria.services.Sistema;
 import cl.ucn.disc.as.conserjeria.services.SistemaImpl;
 import io.ebean.DB;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
 import io.javalin.Javalin;
 import io.javalin.http.NotFoundResponse;
 
@@ -53,6 +59,42 @@ public final class WebController implements RoutesConfigurator {
             ctx.json(oPersona.orElseThrow(() -> new NotFoundResponse("Can't find Persona with rut: " + rut)));
 
         });
+
+        app.get("/api/grpc/personas/rut/{rut}", ctx -> {
+
+            String rut = ctx.pathParam("rut");
+
+            // the channel
+            ManagedChannel channel = ManagedChannelBuilder
+                    .forAddress("127.0.0.1", 50123) // FIXME: declare the port in configuration
+                    .usePlaintext() // FIXME: don't use unencripted protocol
+                    .build();
+
+            // stub
+            PersonaGrpcServiceGrpc.PersonaGrpcServiceBlockingStub stub =
+                    PersonaGrpcServiceGrpc.newBlockingStub(channel);
+
+            // call the gprc
+            PersonaGrpcResponse response = stub.retrieve(PersonaGrpcRequest
+                                                                 .newBuilder()
+                                                                 .setRut("130144918")
+                                                                 .build());
+            // get the response
+            PersonaGrpc personaGrpc = response.getPersona();
+
+            // FIXME: use the mapper to convert domain
+            // return the persona
+            Optional<Persona> oPersona = Optional.of(Persona.builder()
+                                                             .rut(personaGrpc.getRut())
+                                                             .nombre(personaGrpc.getNombre())
+                                                             .apellidos(personaGrpc.getApellidos())
+                                                             .email(personaGrpc.getEmail())
+                                                             .build());
+
+            ctx.json(oPersona.orElseThrow(() -> new NotFoundResponse("Can't find Persona with rut: " + rut)));
+
+        });
+
 
     }
 
